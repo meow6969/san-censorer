84d9af22293856ad8a6faefd346da94cdeb36a23
meow6969 <sdfsdfaasdfdcf@gmail.com>
2021-04-04 14:57:38 -0700

    fix bot

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import json
 from discord import Webhook, AsyncWebhookAdapter
 import git
 import re
+import aiohttp
+
 
 class bcolors:
     HEADER = '\033[95m'
@@ -17,13 +19,10 @@ class bcolors:
     UNDERLINE = '\033[4m'
 
 
-with open("config.json") as meow:
-    prefix = json.load(meow)["prefix"]
+# bot_intents = discord.Intents.default()
+# bot_intents.members = True
 
-bot_intents = discord.Intents.default()
-bot_intents.members = True
-
-client = commands.Bot(command_prefix=prefix, intents=bot_intents, fetch_offline_members=True, case_insensitive=True)
+client = commands.Bot(command_prefix='meow', case_insensitive=True)
 
 
 @client.event
@@ -35,10 +34,36 @@ async def on_ready():
 async def on_message(message):
     print(f'{bcolors.OKCYAN}Message from {message.author}: {message.content}{bcolors.ENDC}')
     with open("config.json") as meow:
-        fmales = json.load(meow["bad-f*males"])
+        fmales = json.load(meow)["bad-f*males"]
         if message.author.id in fmales:
             if re.finditer(r"\b(fumo)(s\b|\b)", message.content, re.IGNORECASE):
                 await message.delete()
+                # async with aiohttp.ClientSession() as session:
+                #     webhook = Webhook.from_url(i.url, adapter=AsyncWebhookAdapter(session))
+                success = False
+                for i in await message.channel.webhooks():
+                    if i.channel == message.channel:
+                        if i.name == '_fumo':
+                            async with aiohttp.ClientSession() as session:
+                                webhook = Webhook.from_url(i.url, adapter=AsyncWebhookAdapter(session))
+                                success = True
+                                if message.author.nick is None:
+                                    nick = message.author
+                                await webhook.send("@everyone i love fumos!!!!",
+                                                   username=str(nick),
+                                                   avatar_url=message.author.avatar_url)
+                if not success:
+                    await message.channel.create_webhook(name="_fumo")
+                    for i in await message.guild.webhooks():
+                        if i.channel == message.channel:
+                            if i.name == '_fumo':
+                                async with aiohttp.ClientSession() as session:
+                                    webhook = Webhook.from_url(i.url, adapter=AsyncWebhookAdapter(session))
+                                    if message.author.nick is None:
+                                        nick = message.author
+                                    await webhook.send("@everyone i love fumos!!!!",
+                                                       username=str(nick),
+                                                       avatar_url=message.author.avatar_url)
 
 
 with open("config.json") as meow:
